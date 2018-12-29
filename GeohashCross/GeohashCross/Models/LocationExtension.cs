3153cbccf2a33aaa63d51eadc488984dd30d42da
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2018-12-29 12:52:13 +1100

    Moved some of the location calculation methods out of the view model and into extension methods.

--- GeohashCross/GeohashCross/Models/LocationExtension.cs
+++ GeohashCross/GeohashCross/Models/LocationExtension.cs
@@ -6,9 +6,9 @@ using Xamarin.Essentials;
 namespace GeohashCross.Models
 {
     public static class LocationExtension
-    { 
-            public static List<Location> GetNeighbours(this Location location)
-            {
+    {
+        public static List<Location> GetNeighbours(this Location location)
+        {
             var hashes = new List<Location>
                 {
                     new Location(location.Latitude -1, location.Longitude -1, location.Timestamp),
@@ -24,6 +24,27 @@ namespace GeohashCross.Models
                 };
             return hashes;
         }
-        
+
+        public static double FixDegreesRange (this double degrees)
+        {
+            if (degrees < 0)
+                return 360 - Math.Abs(degrees);
+            if (degrees > 360)
+                return degrees - 360;
+            return degrees;
+        }
+
+        public static double ToDegrees (this double radians)
+        {
+            var degrees = (180 / Math.PI) * radians;
+            return degrees;
+        }
+
+        public static double ToRadians(this double degrees)
+        {
+            var radians = (Math.PI / 180) * degrees;
+            return radians;
+        }
+
     }
 }
