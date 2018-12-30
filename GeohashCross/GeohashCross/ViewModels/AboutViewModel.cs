a2b048050d4e760d550f840af071d181f6815e02
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2018-12-30 14:25:20 +1100

    Implemented about page using a table view Each entry opens a web page in the system browser using a single command in the ViewModel.
    ToDo: at the moment many of the entries list a display url which is the same as the parameter. Ideally should be refactored so they both reference the same url.

---
+++ GeohashCross/GeohashCross/ViewModels/AboutViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace GeohashCross.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public ICommand WebsiteCommand => new Command<string>(OpenMyWebsite);
+
+        async void OpenMyWebsite(string url)
+        {
+            try
+            {
+                await Xamarin.Essentials.Browser.OpenAsync(url);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"error in opening site\n{ex}\n{ex.StackTrace}");
+            }
+        }
+
+    }
+}
