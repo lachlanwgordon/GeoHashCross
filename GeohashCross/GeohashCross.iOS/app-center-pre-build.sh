51280925fa619436387298de4a76221404e5cef8
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-05-05 15:25:59 +1000

    Added a prebuild script, cleaned up some junk

---
+++ GeohashCross/GeohashCross.iOS/app-center-pre-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "hello"
+
+echo "$MAPS_KEY"
+
+if [ ! -n "$MAPS_KEY" ]
+then
+    echo "You need define the MAPS_KEY variable in App Center"
+    exit
+fi
+
+    echo "Updating Maps_Key to $MAPS_KEY in APIKeys.cs"
+    sed -i '' 's#MapsKey = "YOUR KEY HERE"#MapsKey = "'$MAPS_KEY'"#' ../GeohashCross/Resources/APIKeys.cs
+    echo "File content:"
+    cat ../GeohashCross/Resources/APIKeys.cs
